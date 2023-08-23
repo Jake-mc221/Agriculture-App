c71a7bf88a96c90d351825c5fa1d10bd0ed9046a
Angus O'Grady <angus.ogrady@sydney.edu.au>
2023-08-23 16:15:11 +1000

    Add storybook.js and work on button component, change login to use button.

---
+++ src/components/common/Button.stories.tsx
@@ -0,0 +1,24 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { Button } from "./Button";
+
+const meta: Meta<typeof Button> = {
+  component: Button,
+};
+
+export default meta;
+type Story = StoryObj<typeof Button>;
+
+/*
+ *👇 Render functions are a framework specific feature to allow you control on how the component renders.
+ * See https://storybook.js.org/docs/react/api/csf
+ * to learn how to use render functions.
+ */
+export const Primary: Story = {
+  render: () => (
+    <div className="h-full flex flex-col items-center justify-center gap-5">
+      <Button intent="primary">button</Button>
+      <Button intent="secondary">button</Button>
+    </div>
+  ),
+};
---
+++ src/components/common/Button.tsx
@@ -0,0 +1,45 @@
+import { VariantProps, cva } from "class-variance-authority";
+import {
+  ComponentPropsWithoutRef,
+  ElementType,
+  PropsWithChildren,
+} from "react";
+import { twMerge } from "tailwind-merge";
+
+const buttonStyles = cva("transition-transform motion-safe:active:scale-95", {
+  variants: {
+    intent: {
+      unstyled: "",
+      primary:
+        "bg-primary text-white min-w-[10rem] rounded-l-full rounded-r-full p-2 hover:bg-primary-hover flex items-center justify-center",
+      secondary:
+        "bg-white text-primary min-w-[10rem] rounded-l-full rounded-r-full p-2 border-2 border-primary hover:bg-gray-200 flex items-center justify-center",
+    },
+  },
+  defaultVariants: {
+    intent: "primary",
+  },
+});
+
+export function Button<T extends ElementType>({
+  component,
+  children,
+  className,
+  ...buttonProps
+}: { component?: T } & PropsWithChildren &
+  ComponentPropsWithoutRef<T> &
+  VariantProps<typeof buttonStyles>) {
+  const Component: ElementType = component || "button";
+
+  return (
+    <Component
+      {...(buttonProps as ComponentPropsWithoutRef<T>)}
+      className={twMerge(
+        buttonStyles(buttonProps as VariantProps<typeof buttonStyles>),
+        className,
+      )}
+    >
+      {children}
+    </Component>
+  );
+}
