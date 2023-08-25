cb5bc264812c67308df74eaa994917c41cf44663
Chen Kang Yang <kche0105@uni.sydney.edu.au>
2023-08-25 17:36:52 +1000

    Fix build

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from "@capacitor/cli";
 const config: CapacitorConfig = {
   appId: "com.uni.sydney.comp3888.p30",
   appName: "Ag. Data App",
-  webDir: "src",
+  webDir: "out",
   server: {
     androidScheme: "https",
   },
