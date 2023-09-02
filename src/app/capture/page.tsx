c23475a59379c39d951d509c0756bbee6ce1bcab
SillySuli <sulavmalla.malla7@gmail.com>
2023-09-03 01:31:09 +1000

    add image saving to capture page

--- src/app/capture/page.tsx
+++ src/app/capture/page.tsx
@@ -3,18 +3,78 @@
 import { Button } from "@/components/common/Button";
 import { PageContainer } from "@/components/common/PageContainer";
 import { Select } from "@/components/common/Select";
-import {
-  Camera,
-  CameraResultType,
-  Photo,
-  FilesystemDirectory,
-} from "@capacitor/camera";
+import { Camera, CameraResultType, Photo } from "@capacitor/camera";
+import { Capacitor } from "@capacitor/core";
+import { Filesystem, Directory } from "@capacitor/filesystem";
 import { useState } from "react";
 import { TbPhoto } from "react-icons/tb";
 
 export default function Home() {
   const [image, setImage] = useState<Photo>();
 
+  {
+    /** Helpful funciton for saving the images */
+  }
+
+  const readAsBase64 = async (photo: Photo) => {
+    // Fetching the photo, reading it as a blob and then converting it into base 64 format.
+    if (Capacitor.isNativePlatform()) {
+      const file = await Filesystem.readFile({
+        path: photo.path as string,
+      });
+      return file.data;
+    } else {
+      const response = await fetch(photo.webPath as string);
+      const blob = await response.blob();
+      return (await convertBlobToBase64(blob)) as string;
+    }
+  };
+
+  const convertBlobToBase64 = (blob: Blob) =>
+    new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onerror = reject;
+      reader.onload = () => {
+        resolve(reader.result);
+      };
+      reader.readAsDataURL(blob);
+    });
+
+  const savePhoto = async (photo: Photo) => {
+    // conver the phtoto to a base64 format, required by the Filesystem API to save.
+    const base64Data = await readAsBase64(photo);
+
+    // Wrtie the data into the data directory.
+    const filename = Date.now() + ".jpeg";
+    const savedfile = await Filesystem.writeFile({
+      path: filename,
+      data: base64Data,
+      directory: Directory.Data,
+    });
+
+    // To view the immage, load it from the webpath since its already loaded into memory
+    return {
+      filepath: filename,
+      webviewPath: photo.webPath,
+    };
+  };
+
+  const takePhoto = async () => {
+    try {
+      setImage(
+        await Camera.getPhoto({
+          quality: 90,
+          allowEditing: false,
+          resultType: CameraResultType.DataUrl,
+        }),
+      );
+      if (image) {
+        await savePhoto(image);
+      }
+    } catch (e) {
+      return;
+    }
+  };
   return (
     <PageContainer>
       <div className="w-full flex-grow flex flex-col gap-5">
@@ -32,19 +92,7 @@ export default function Home() {
           </div>
         )}
 
-        <Button
-          onClick={async () => {
-            setImage(
-              await Camera.getPhoto({
-                quality: 90,
-                allowEditing: false,
-                resultType: CameraResultType.DataUrl,
-              }),
-            );
-          }}
-        >
-          Capture
-        </Button>
+        <Button onClick={takePhoto}>Capture</Button>
 
         <Select label="Crop Type">
           <option>Crop1</option>
