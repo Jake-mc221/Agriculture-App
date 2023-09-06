08ac8bf998edcff7a55b18d2d92b49a472b95a4f
Marian Baher Prince Shaker Derias <mder3758@uni.sydney.edu.au>
2023-09-06 17:44:26 +1000

    made navbar highlight correct tabs based on path

--- src/components/core/NavigationBar.tsx
+++ src/components/core/NavigationBar.tsx
@@ -7,13 +7,11 @@ import { LuScanLine } from "react-icons/lu";
 import { MdAddChart } from "react-icons/md";
 import Link from "next/link";
 import { Button } from "../common/Button";
-import { SetStateAction, useState } from "react";
+import { usePathname } from "next/navigation";
 
 export default function NavigationBar() {
-  const [activeLink, setActiveLink] = useState("home"); // current active link is home
-  const changeActive = (link: SetStateAction<string>) => {
-    setActiveLink(link);
-  };
+  const path = usePathname();
+
   return (
     <>
       <div className="h-36 w-full">
@@ -22,18 +20,20 @@ export default function NavigationBar() {
             intent="unstyled"
             component={Link}
             href="/capture"
-            className=" self-center z-10 flex justify-center items-center h-20 w-20 rounded-full shadow-md absolute bottom-24 bg-gradient-to-t from-primary to-green-400"
+            className={
+              "self-center z-10 flex justify-center items-center h-20 w-20 rounded-full shadow-md absolute bottom-24 bg-gradient-to-t from-primary to-green-300" +
+              (path == "/capture" ? "from-primary to-green-500" : "")
+            }
           >
-            <LuScanLine className="text-3xl text-gray-600" />
+            <LuScanLine className="text-4xl text-gray-600" />
           </Button>
 
           <div className="flex-1 flex flex-row justify-around items-center border border-t border-slate-10">
             <Button
               className={
                 "w-full h-full rounded-md flex " +
-                (activeLink === "home" ? "bg-slate-200" : "")
+                (path === "/search" ? "bg-slate-200" : "")
               }
-              onClick={() => changeActive("home")}
               intent="unstyled"
               component={Link}
               href="/search"
@@ -43,9 +43,8 @@ export default function NavigationBar() {
             <Button
               className={
                 "w-full h-full rounded-md flex " +
-                (activeLink === "add" ? "bg-slate-200" : "")
+                (path === "/label" ? "bg-slate-200" : "")
               }
-              onClick={() => changeActive("add")}
               intent="unstyled"
               component={Link}
               href="/label"
@@ -55,9 +54,8 @@ export default function NavigationBar() {
             <Button
               className={
                 "w-full h-full rounded-md flex " +
-                (activeLink === "leaderboard" ? "bg-slate-200" : "")
+                (path === "/leaderboard" ? "bg-slate-200" : "")
               }
-              onClick={() => changeActive("leaderboard")}
               intent="unstyled"
               component={Link}
               href="/leaderboard"
@@ -67,9 +65,8 @@ export default function NavigationBar() {
             <Button
               className={
                 "w-full h-full rounded-md flex " +
-                (activeLink === "profile" ? "bg-slate-200" : "")
+                (path === "/profile" ? "bg-slate-200" : "")
               }
-              onClick={() => changeActive("profile")}
               intent="unstyled"
               component={Link}
               href="/profile"
