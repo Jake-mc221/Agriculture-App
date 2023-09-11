14db2080738bade99ccc1e3d9df9aabc6922d64b
Belinda Hu <behu3179@uni.sydney.edu.au>
2023-09-11 22:40:47 +1000

    Updated capture guidelines with new images and formatting

--- src/app/guidelines/page.tsx
+++ src/app/guidelines/page.tsx
@@ -8,55 +8,61 @@ export default function CaptureGuidelines() {
   return (
     <div className="flex flex-col items-center w-full h-full">
       <h1 className="m-5">Capture Guidelines</h1>
-      <div className="flex flex-col items-center w-full h-full">
+
+      <div className="flex flex-col items-center w-full h-full m-2">
         <img
           src="/images/cauliflower.jpg"
-          alt="Cauliflower"
-          className="rounded-3xl w-80 mx-auto"
+          alt="Photo of cauliflower plant from bird's eye view."
+          className="rounded-3xl w-64 m-2"
+        />
+        <img
+          src="/images/cauliflower_seedling.jpg"
+          alt="Photo of cauliflower seeding from bird's eye view."
+          className="rounded-3xl w-64 m-2"
         />
         <div className="bg-green-500 rounded-full p-2 mt-2">
           <TiTick className="text-white" />
         </div>
-        <p className="m-5 text-center">Good Photo</p>
+        <p className="mt-2 font-bold text-lg">Full crop from overhead</p>
       </div>
 
-      <div className="flex justify-between mx-2 space-x-5">
-        <div className="flex flex-col items-center w-full h-full">
+      <div className="flex justify-between m-2 space-x-5">
+        <div className="flex flex-col items-center h-40">
           <img
             src="/images/cauliflower_far.jpg"
-            alt="Crop photo from too far away."
-            className="rounded-3xl w-40 h-30"
+            alt="Photo of cauliflower field."
+            className="rounded-3xl w-full h-full object-cover"
           />
           <div className="bg-red-500 rounded-full p-2 mt-2">
             <RxCross2 className="text-white" />
           </div>
-          <p className="mt-2">Too Far Away</p>
+          <p className="mt-2">Too far away</p>
         </div>
-        <div className="flex flex-col items-center w-full h-full">
+        <div className="flex flex-col items-center h-40">
           <img
             src="/images/cauliflower_close.jpg"
-            alt="Crop photo from too close."
-            className="rounded-3xl w-40 h-30"
+            alt="Photo of close up cauliflower plant."
+            className="rounded-3xl w-full h-full object-cover"
           />
           <div className="bg-red-500 rounded-full p-2 mt-2">
             <RxCross2 className="text-white" />
           </div>
-          <p className="mt-2">Too Close</p>
+          <p className="mt-2">Too close</p>
         </div>
-        <div className="flex flex-col items-center w-full h-full">
+        <div className="flex flex-col items-center h-40">
           <img
-            src="/images/multiple_veges.jpg"
-            alt="Multiple crop types"
-            className="rounded-3xl w-40 h-30"
+            src="/images/cauliflower_low_angle.jpg"
+            alt="Photo of cauliflower seedling from a low angle."
+            className="rounded-3xl w-full h-full object-cover"
           />
           <div className="bg-red-500 rounded-full p-2 mt-2">
             <RxCross2 className="text-white" />
           </div>
-          <p className="mt-2">Multiple Crop Types</p>
+          <p className="mt-2">Wrong angle</p>
         </div>
       </div>
 
-      <Button className="m-10" component={Link} href="/capture">
+      <Button className="m-20" component={Link} href="/capture">
         Continue
       </Button>
     </div>
