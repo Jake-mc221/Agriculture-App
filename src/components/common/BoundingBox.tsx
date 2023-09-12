10929daca98140ca4e54d22189dd8afef68bd866
Marian Baher Prince Shaker Derias <mder3758@uni.sydney.edu.au>
2023-09-12 11:17:51 +1000

    made the bounding box a little cleaner

--- src/components/common/BoundingBox.tsx
+++ src/components/common/BoundingBox.tsx
@@ -12,52 +12,10 @@ export default function BoundingBox({ image }: BoundingProps) {
   const [isResizing, setIsResizing] = useState(false); // records whether the bounding box is being resized or not
   const [dimensions, setDimensions] = useState({ x: 50, y: 50 }); // records the current width and height of the bounding box
   const [mouseDim, setMouseDim] = useState({ x: 0, y: 0 }); // records the most recent click positions of the mouse before dragging
-  const resize = (
-    e: React.MouseEvent<HTMLDivElement, MouseEvent>,
-    corner: string,
-    left: number,
-    top: number,
-  ) => {
+
+  const resize = (x: number, y: number) => {
     const image = document.getElementById("image")?.getBoundingClientRect();
     const box = document.getElementById("box");
-    const width = dimensions.x + left * (e.clientX - mouseDim.x);
-    const height = dimensions.y + top * (e.clientY - mouseDim.y);
-    if (
-      !isResizing ||
-      image == null ||
-      box == null ||
-      image.left > e.clientX ||
-      image.right < e.clientX ||
-      image.top > e.clientY ||
-      image.bottom < e.clientY
-    ) {
-      return;
-    }
-    // Set the new width and height of the box
-    const minimum_size = 50;
-    if (width > minimum_size && height > minimum_size) {
-      setDimensions({ x: width, y: height });
-    } else if (width > minimum_size) {
-      setDimensions({ x: width, y: dimensions.y });
-    } else if (height > minimum_size) {
-      setDimensions({ x: dimensions.x, y: height });
-    }
-    // Set the new position of the box based on which corner you are coming from
-    box.style.transform =
-      "translate(calc(" +
-      (mouseDim.x + image.left) +
-      "px , calc(" +
-      (mouseDim.y + image.top) +
-      "px))";
-
-    setMouseDim({ x: e.clientX, y: e.clientY });
-  };
-
-  const resizePhone = (e: React.TouchEvent<HTMLDivElement>) => {
-    const image = document.getElementById("image")?.getBoundingClientRect();
-    const box = document.getElementById("box");
-    const x = e.touches[0].clientX;
-    const y = e.touches[0].clientY;
     const width = dimensions.x + (x - mouseDim.x);
     const height = dimensions.y + (y - mouseDim.y);
 
@@ -88,6 +46,7 @@ export default function BoundingBox({ image }: BoundingProps) {
       "px , calc(" +
       (mouseDim.y + image.top) +
       "px))";
+
     setMouseDim({ x: x, y: y });
   };
 
@@ -109,22 +68,22 @@ export default function BoundingBox({ image }: BoundingProps) {
       >
         {/*This is a little ugly but I had to put in these divs to show the grid. If I used an image or smthg the scaling would get messed up during resizing */}
         <div className="grid grid-cols-4 h-full w-full">
-          <div className="border rounded border-gray-500" />
-          <div className="border rounded border-gray-500" />
-          <div className="border rounded border-gray-500" />
-          <div className="border rounded border-gray-500" />
-          <div className="border rounded border-gray-500" />
-          <div className="border rounded border-gray-500" />
-          <div className="border rounded border-gray-500" />
-          <div className="border rounded border-gray-500" />
-          <div className="border rounded border-gray-500" />
-          <div className="border rounded border-gray-500" />
-          <div className="border rounded border-gray-500" />
-          <div className="border rounded border-gray-500" />
-          <div className="border rounded border-gray-500" />
-          <div className="border rounded border-gray-500" />
-          <div className="border rounded border-gray-500" />
-          <div className="border rounded border-gray-500" />
+          <div className="border rounded opacity-5 border-gray-500" />
+          <div className="border rounded opacity-5 border-gray-500" />
+          <div className="border rounded opacity-5 border-gray-500" />
+          <div className="border rounded opacity-5 border-gray-500" />
+          <div className="border rounded opacity-5 border-gray-500" />
+          <div className="border rounded opacity-5 border-gray-500" />
+          <div className="border rounded opacity-5 border-gray-500" />
+          <div className="border rounded opacity-5 border-gray-500" />
+          <div className="border rounded opacity-5 border-gray-500" />
+          <div className="border rounded opacity-5 border-gray-500" />
+          <div className="border rounded opacity-5 border-gray-500" />
+          <div className="border rounded opacity-5 border-gray-500" />
+          <div className="border rounded opacity-5 border-gray-500" />
+          <div className="border rounded opacity-5 border-gray-500" />
+          <div className="border rounded opacity-5 border-gray-500" />
+          <div className="border rounded opacity-5 border-gray-500" />
         </div>
 
         <div
@@ -132,7 +91,7 @@ export default function BoundingBox({ image }: BoundingProps) {
           onMouseUp={() => {
             setIsResizing(false);
           }}
-          onDrag={(e) => resize(e, "bottom-right", 1, 1)}
+          onDrag={(e) => resize(e.clientX, e.clientY)}
           onMouseDown={(e) => {
             setIsResizing(true);
             setMouseDim({ x: e.pageX, y: e.pageY });
@@ -146,7 +105,9 @@ export default function BoundingBox({ image }: BoundingProps) {
               y: e.touches[0].clientY,
             });
           }}
-          onTouchMove={(e) => resizePhone(e)}
+          onTouchMove={(e) =>
+            resize(e.touches[0].clientX, e.touches[0].clientY)
+          }
           onTouchEnd={() => {
             setIsResizing(false);
           }}
@@ -154,7 +115,7 @@ export default function BoundingBox({ image }: BoundingProps) {
           style={{
             clipPath:
               "polygon(78% 78%, 78% 0, 100% 0, 100% 100%, 0 100%, 0 78%)",
-          }}// I wanted to do it like this bc the alternative would be to add a  css file to get this kind of preciseness: https://stackoverflow.com/questions/68932695/how-to-add-a-clip-path-to-image-in-tailwind
+          }} // I wanted to do it like this bc the alternative would be to add a  css file to get this kind of preciseness: https://stackoverflow.com/questions/68932695/how-to-add-a-clip-path-to-image-in-tailwind
         ></div>
       </motion.div>
     </div>
